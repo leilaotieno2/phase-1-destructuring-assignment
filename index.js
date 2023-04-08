df48fd359c98b822cb8ce2005ba0c9e3f9f0706b
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-08 18:55:18 +0300

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const farmAnimals = 'cow horse sheep pig chicken';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
+// Objects
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -27,22 +28,33 @@ const nestedMuppet = {
 
 // Strings
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+// 1. Use destructuring to declare `red`, `blue`, `green`. 
+const [red, blue, green] = ['cow', 'sheep', 'duck'];
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+// 2. Use destructuring to declare `firstName`, `lastName` and `gender` with default value `unknown`.
+const [firstName = 'unknown', lastName = 'unknown', gender = 'unknown'] = ['Miss', 'Piggy'];
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+// 3. Use destructuring to assign `animal`, `song` and `profession` from the given object.
+const {animal, song, profession} = {
+  animal: 'bear',
+  song: 'Rainbow Connection',
+  profession: 'host of The Muppet Show'
+};
 
-// Arrays
+// 4. Use destructuring to assign `fruit` and `vegetable` from the given array.
+const [fruit, vegetable] = ['apple', 'broccoli', 'carrot'];
 
-// 4. Use destructuring to assign appropriate variables using the color names.
+// 5. Use destructuring to swap `a` and `b`.
+let a = 5;
+let b = 7;
+[a, b] = [b, a];
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+// 6. Use destructuring with rest to assign the first value to `first`, the second value to `second` and the rest of the values to `otherColors`.
+const [first, second, ...otherColors] = ['purple', 'yellow', 'red', 'green', 'orange'];
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 7. Use destructuring with rest to assign the first value to `name` and the rest of the values to `titles`.
+const [name, ...titles] = ['Kermit', 'host of The Muppet Show', 'star of The Muppet Movie'];
 
-// Objects
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner.
+const {album: {theMuppetMovie: {song2, song4}}, job: {nestedJob: job2}, partner: {nestedPartner: partner2}} = nestedMuppet;
 
-// 7. Use destructuring to assign all variables using the keys as the variable names
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
